d54e97c7ce3b81e3cacf4070c1a60ead3532b434
Tom Hindby <46819924+UpperCenter@users.noreply.github.com>
2021-04-04 23:28:28 +0100

    Added Better Meta Tags

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+		
+		<!-- Primary Meta Tags -->
+		<title>Oberon - The Blazing Fast Cyber Range</title>
+		<meta name="title" content="Oberon - The Blazing Fast Cyber Range">
+		<meta name="description" content="Welcome to the online portal for the Oberon Cyber Range.">
+
+		<!-- Open Graph / Facebook -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://metatags.io/">
+		<meta property="og:title" content="Oberon - The Blazing Fast Cyber Range">
+		<meta property="og:description" content="Welcome to the online portal for the Oberon Cyber Range.">
+		<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+		<!-- Twitter -->
+		<meta property="twitter:card" content="summary_large_image">
+		<meta property="twitter:url" content="https://metatags.io/">
+		<meta property="twitter:title" content="Oberon - The Blazing Fast Cyber Range">
+		<meta property="twitter:description" content="Welcome to the online portal for the Oberon Cyber Range.">
+		<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
