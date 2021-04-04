5d696f07d8982290a857147b6ada24838705ff7f
Tom Hindby <46819924+UpperCenter@users.noreply.github.com>
2021-04-04 23:25:24 +0100

    Added Better Meta Tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,28 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- Primary Meta Tags -->
+		<title>Oberon - The Blazing Fast Cyber Range</title>
+		<meta name="title" content="Oberon - The Blazing Fast Cyber Range">
+		<meta name="description" content="Welcome to the online portal for the Oberon Cyber Range.">
 
-        <title>Laravel</title>
+		<!-- Open Graph / Facebook -->
+		<meta property="og:type" content="website">
+		<meta property="og:url" content="https://metatags.io/">
+		<meta property="og:title" content="Oberon - The Blazing Fast Cyber Range">
+		<meta property="og:description" content="Welcome to the online portal for the Oberon Cyber Range.">
+		<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+		<!-- Twitter -->
+		<meta property="twitter:card" content="summary_large_image">
+		<meta property="twitter:url" content="https://metatags.io/">
+		<meta property="twitter:title" content="Oberon - The Blazing Fast Cyber Range">
+		<meta property="twitter:description" content="Welcome to the online portal for the Oberon Cyber Range.">
+		<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+
+        <title>Oberon Cyber Range</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
