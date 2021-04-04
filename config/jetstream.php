cb5fcf0076b0caf76f67df80e6468512180d38a6
UpperCenter <46819924+UpperCenter@users.noreply.github.com>
2021-04-04 16:36:15 +0100

    Updated Cookie Security

--- config/jetstream.php
+++ config/jetstream.php
@@ -45,7 +45,7 @@ return [
 		Features::profilePhotos(),
 		// Features::api(),
 		// Features::teams(['invitations' => true]),
-		// Features::accountDeletion(),
+		Features::accountDeletion(),
 	],
 
 	/*
