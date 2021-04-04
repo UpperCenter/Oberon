4d81833e175935359b04b2903199537b458a3c50
UpperCenter <46819924+UpperCenter@users.noreply.github.com>
2021-04-04 22:44:46 +0100

    Testing Localhost Images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,17 +12,17 @@
 
 		<!-- Open Graph / Facebook -->
 		<meta property="og:type" content="website">
-		<meta property="og:url" content="https://metatags.io/">
+		<meta property="og:url" content="https://cyber.lol/">
 		<meta property="og:title" content="Oberon - The Blazing Fast Cyber Range">
 		<meta property="og:description" content="Welcome to the online portal for the Oberon Cyber Range.">
-		<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+		<meta property="og:image" content="asset('images/meta.webp');">
 
 		<!-- Twitter -->
 		<meta property="twitter:card" content="summary_large_image">
-		<meta property="twitter:url" content="https://metatags.io/">
+		<meta property="twitter:url" content="https://cyber.lol/">
 		<meta property="twitter:title" content="Oberon - The Blazing Fast Cyber Range">
 		<meta property="twitter:description" content="Welcome to the online portal for the Oberon Cyber Range.">
-		<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+		<meta property="twitter:image" content="asset('images/meta.webp');">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
