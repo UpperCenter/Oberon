8213985827ec9c417cc10905f49d8c43589b709f
UpperCenter <46819924+UpperCenter@users.noreply.github.com>
2021-05-06 00:45:01 +0100

    Added New Users

--- resources/views/administrator/users/index.blade.php
+++ resources/views/administrator/users/index.blade.php
@@ -13,9 +13,9 @@
                             <table class="w-full min-w-full divide-y divide-gray-200">
                                 <thead>
                                     <tr>
-                                        <th scope="col" width="50"
+                                        <th scope="col" width="100"
                                             class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase bg-gray-50">
-                                            ID
+                                            User ID
                                         </th>
                                         <th scope="col"
                                             class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase bg-gray-50">
@@ -59,6 +59,70 @@
                                             <span
                                                 class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
                                                 Administrator
+                                            </span>
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm font-medium whitespace-nowrap">
+                                            <a href="#" class="mb-2 mr-2 text-blue-600 hover:text-blue-900">View</a>
+                                            <a href="#" class="mb-2 mr-2 text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            <a href="#" class="mb-2 mr-2 text-red-600 hover:text-red-900">Delete</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            2
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            James Cooper
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            james@gmail.com
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            Saturday 1st May 2021 - 05:15
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            <span
+                                                class="inline-flex px-2 text-xs font-semibold leading-5 text-black bg-yellow-200 rounded-full">
+                                                Lecturer
+                                            </span>
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm font-medium whitespace-nowrap">
+                                            <a href="#" class="mb-2 mr-2 text-blue-600 hover:text-blue-900">View</a>
+                                            <a href="#" class="mb-2 mr-2 text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            <a href="#" class="mb-2 mr-2 text-red-600 hover:text-red-900">Delete</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            3
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            Sarah Davidson
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            jumpingjelly@hotmail.com
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            Wednesday 5th May 2021 - 03:13
+                                        </td>
+
+                                        <td class="px-6 py-4 text-sm text-gray-900 whitespace-nowrap">
+                                            <span
+                                                class="inline-flex px-2 text-xs font-semibold leading-5 text-purple-800 bg-purple-100 rounded-full">
+                                                Student
                                             </span>
                                         </td>
 
